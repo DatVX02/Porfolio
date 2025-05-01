7b1e741979e4aedcb488f4b36cdd41d8c299b4ad
DatVX02 <vxdat13@gmail.com>
2025-05-01 15:17:12 +0700

    create experience

--- Porfolio/src/pages/Experience/Experience.jsx
+++ Porfolio/src/pages/Experience/Experience.jsx
@@ -1,10 +1,20 @@
 import React from "react";
-
+import logo_amazing from "../../assets/logo-color.png";
 const Experience = () => {
     return (
-        <div className="bg-gradient-to-br from-blue-50 to-gray-100">
-            <h1 className="text-center text-4xl font-bold uppercase p-10">Experience</h1>
+        <div className="bg-gradient-to-br from-blue-100 to-gray-200  py-12" id="skills">
+            <div className="text-center uppercase font-bold text-4xl md:text-5xl text-gray-800 tracking-wide">
+                <h1 className="relative inline-block mb-20">
+                    Experience
+                    <span className="absolute -bottom-2 left-0 w-full h-1 bg-blue-500 rounded-full"></span>
+                </h1>
+            </div>
+            <div>
+                <img src={logo_amazing} alt="Amazing Tech Company"/>
+            </div>
         </div>
+
+
     )
 }
 export default Experience;
